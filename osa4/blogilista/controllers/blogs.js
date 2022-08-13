79d2cb3b6f3704ce72aebc68e4e6ba8153a99bed
antonlep <antonleppa@gmail.com>
2022-08-13 16:03:32 +0300

    part 4.22

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,23 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', middleware.userExtractor, async (request, response, next) => {
+    const body = request.body
+    const user = request.user
+    body.user = user._id
+    const blog = new Blog(body)
 
     try {
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(201).json(savedBlog)
     } catch (exception) {
         next(exception)
     }
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+    const user = request.user
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+        return response.status(400).json({
+            error: 'blog doesnt exist'
+        })
+    }
+    if (user.id.toString() !== blog.user.toString()) {
+        return response.status(401).json({ error: 'only creator can delete a blog' })
+    }
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
 })
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
     logger.info('Method:', request.method)
@@ -7,6 +9,31 @@ const requestLogger = (request, response, next) => {
     logger.info('---')
     next()
 }
+
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    }
+    next()
+}
+
+const userExtractor = async (request, response, next) => {
+    if (!request.token) {
+        return response.status(401).json({ error: 'token missing' })
+    }
+    try {
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        if (!decodedToken.id) {
+            return response.status(401).json({ error: 'token invalid' })
+        }
+        request.user = await User.findById(decodedToken.id)
+    } catch (exception) {
+        next(exception)
+    }
+    next()
+}
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -27,5 +54,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    tokenExtractor,
+    userExtractor
 }
