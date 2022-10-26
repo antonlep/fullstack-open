07e524cb68cc6249feeb4c8265222f1b13f4a749
antonlep <antonleppa@gmail.com>
2022-10-26 20:57:02 +0300

    part 9.17

---
+++ osa9/patientor-backend/build/src/services/patientService.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const patients_1 = __importDefault(require("../../data/patients"));
+const uuid_1 = require("uuid");
+const getEntries = () => {
+    return patients_1.default;
+};
+const getEntry = (id) => {
+    return patients_1.default.find(a => a.id === id);
+};
+const getSensitiveEntries = () => {
+    return patients_1.default.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation,
+    }));
+};
+const addPatient = (entry) => {
+    const id = (0, uuid_1.v1)();
+    const newPatient = Object.assign({ id: id }, entry);
+    patients_1.default.push(newPatient);
+    return newPatient;
+};
+exports.default = {
+    getEntries,
+    getSensitiveEntries,
+    addPatient,
+    getEntry
+};
